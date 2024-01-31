dbd1c6542df2f72a08e1a56137bf36af7838c5f6
Pavithrannagarajan <138995301+Pavithrannagarajan@users.noreply.github.com>
2024-01-31 17:16:44 +0530

    C

---
+++ JS/script.js
@@ -0,0 +1,118 @@
+// Navbar toggle
+
+
+
+// $(function(){
+//     $("#form-total").steps({
+//         headerTag: "h2",
+//         bodyTag: "section",
+//         transitionEffect: "fade",
+//         enableAllSteps: true,
+//         stepsOrientation: "vertical",
+//         autoFocus: true,
+//         transitionEffectSpeed: 500,
+//         titleTemplate : '<div class="title">#title#</div>',
+//         labels: {
+//             previous : 'Back Step',
+//             next : 'Next',
+//             finish : 'Submit',
+//             current : ''
+//         },
+//     })
+// });
+
+
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+
+
+
+
+
+
+// NAvbar BG 
+
+let navbarBg = document.querySelector('.top-nav');
+let navBtn = document.querySelector('.navbar-toggler');
+
+window.addEventListener('scroll', () => {
+
+  if (window.scrollY >= 90) {
+    navbarBg.classList.add('nav-background');
+  } else if (window.scrollY < 90) {
+    navbarBg.classList.remove('nav-background');
+
+  }
+
+});
+
+navBtn.addEventListener('click', () => {
+
+  if (navbarBg.classList.add('nav-background')) {
+    navbarBg.classList.remove('nav-background');
+
+  } else {
+    navbarBg.classList.add('nav-background');
+
+  }
+
+})
+
+
+
+
+    //////////// Format the message content/////////////
+
+
+    
+    
+    
+    
+        //////////// Format the message content/////////////
+    
+   
+function whatsapp1() {
+   
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value
+    var number = document.getElementById('number').value;
+
+
+
+
+
+//     //////////// Format the message content/////////////
+
+    var formattedMessage = (
+        " *Name* : " + name + "\n" +
+        " *Location* : " + email + "\n" +
+        " *Phone Number* : " + number);
+
+
+    // Encode the message for the URL
+    var encodedMessage = encodeURIComponent(formattedMessage);
+
+    // Construct the WhatsApp URL
+
+    var url = "https://wa.me/+919543115004?text=" + encodedMessage;
+
+
+    // Open the WhatsApp URL in a new tab/window
+    window.open(url);
+
+
+}
+
+
+
+
+
+
+
+
